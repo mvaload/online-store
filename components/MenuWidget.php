51ff64245db34319e14eb4fb018fb1322f621642
Владимир Моисеенко <v.moiseenko@telecontact.ru>
2020-05-06 18:19:12 +0300

    added components and assets and models

---
+++ components/MenuWidget.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace app\components;
+use yii\base\Widget;
+use app\models\Category;
+
+class MenuWidget extends Widget
+{
+    public $tpl;
+    private $data;
+    private $tree;
+    private $menuHtml;
+
+    public function init()
+    {
+        parent::init();
+        if ($this->tpl === null) {
+            $this->tpl = 'menu';
+        }
+        $this->tpl .= '.php';
+    }
+
+    public function run()
+    {
+        $this->data = Category::find()->indexBy('id')->asArray()->all();
+        $this->tree = $this->getTree();
+        $this->menuHtml = $this->getMenuHtml($this->tree);
+        return $this->menuHtml;
+    }
+
+    protected function getTree()
+    {
+        $tree = [];
+        foreach ($this->data as $id => &$node) {
+            if (!$node['parent_id']) {
+                $tree[$id] = &$node;
+            } else {
+                $this->data[$node['parent_id']]['childs'][$node['id']] = &$node;
+            }
+        }
+        return $tree;
+    }
+
+    protected function getMenuHtml($tree)
+    {
+        $str = '';
+        foreach ($tree as $category) {
+            $str .= $this->catToTemplate($category);
+        }
+        return $str;
+    }
+
+    protected function catToTemplate($category)
+    {
+        ob_start();
+        include __DIR__ . DIRECTORY_SEPARATOR . "menu_tpl". DIRECTORY_SEPARATOR . $this->tpl;
+        return ob_get_clean();
+    }
+
+}
---
+++ components/menu_tpl/select.php
@@ -0,0 +1,11 @@
+<ul>
+    <li><a href="">Item 1</a></li>
+    <li><a href="">Item 2</a>
+        <ul>
+            <li></li>
+        </ul>
+    </li>
+    <li><a href="">Item 3</a></li>
+    <li><a href="">Item 4</a></li>
+    <li><a href="">Item 5</a></li>
+</ul>
