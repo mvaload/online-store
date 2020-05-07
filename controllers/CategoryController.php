25ca8e5fe8111d20fbfba515784e086896a487dd
mvaload <mvaload@gmail.com>
2020-05-07 17:59:25 +0300

    added pagination

--- controllers/CategoryController.php
+++ controllers/CategoryController.php
@@ -6,6 +6,7 @@ namespace app\controllers;
 use Yii;
 use app\models\Category;
 use app\models\Product;
+use yii\data\Pagination;
 
 class CategoryController extends AppController
 {
@@ -19,10 +20,17 @@ class CategoryController extends AppController
     public function actionView($id)
     {
         $id = Yii::$app->request->get('id');
-        $products = Product::find()->where(['category_id' => $id])->all();
+        $query = Product::find()->where(['category_id' => $id]);
+        $pages = new Pagination([
+            'totalCount' => $query->count(),
+            'pageSize' => 3,
+            'forcePageParam' => false,
+            'pageSizeParam' => false
+        ]);
+        $products = $query->offset($pages->offset)->limit($pages->limit)->all();
         $category = Category::findOne($id);
         $this->setMeta("E-SHOPPER | " . $category->name, $category->keywords, $category->description);
-        return $this->render('view', compact('products', 'category'));
+        return $this->render('view', compact('products', 'pages', 'category'));
     }
 
 }
--- views/category/view.php
+++ views/category/view.php
@@ -2,6 +2,7 @@
 
 use app\components\MenuWidget;
 use yii\helpers\Html;
+use \yii\widgets\LinkPager;
 
 ?>
 <section>
@@ -56,16 +57,18 @@ use yii\helpers\Html;
                                 <div class="clearfix"></div>
                             <?php endif; ?>
                         <?php endforeach; ?>
+                        <div class="clearfix"></div>
+                        <?= LinkPager::widget(['pagination' => $pages ]); ?>
                     <?php else: ?>
                         <h2>Товаров пока нет</h2>
                     <?php endif; ?>
-                    <div class="clearfix"></div>
-                    <ul class="pagination">
+
+                    <!--<ul class="pagination">
                         <li class="active"><a href="">1</a></li>
                         <li><a href="">2</a></li>
                         <li><a href="">3</a></li>
                         <li><a href="">&raquo;</a></li>
-                    </ul>
+                    </ul>-->
                 </div><!--features_items-->
 
             </div>
