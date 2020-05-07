cca256a75431628c93980e03b8109f3e1f8b83d2
Владимир Моисеенко <v.moiseenko@telecontact.ru>
2020-05-07 11:18:13 +0300

    changed widget and added caching for category menu

--- components/MenuWidget.php
+++ components/MenuWidget.php
@@ -1,8 +1,10 @@
 <?php
 
 namespace app\components;
+
 use yii\base\Widget;
 use app\models\Category;
+use Yii;
 
 class MenuWidget extends Widget
 {
@@ -22,9 +24,18 @@ class MenuWidget extends Widget
 
     public function run()
     {
+        // get cache
+        $menu = Yii::$app->cache->get('menu');
+        if ($menu) {
+            return $menu;
+        }
+
         $this->data = Category::find()->indexBy('id')->asArray()->all();
         $this->tree = $this->getTree();
         $this->menuHtml = $this->getMenuHtml($this->tree);
+
+        // set cache
+        Yii::$app->cache->set('menu', $this->menuHtml, 60 * 60);
         return $this->menuHtml;
     }
 
