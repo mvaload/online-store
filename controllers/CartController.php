9e296c445bf83ba46a172da35d0c91064bc531e2
mvaload <mvaload@gmail.com>
2020-05-09 19:45:22 +0300

    added controller cart

---
+++ controllers/CartController.php
@@ -0,0 +1,36 @@
+<?php
+
+
+namespace app\controllers;
+
+use Yii;
+use app\models\Cart;
+use app\models\Product;
+
+
+class CartController extends AppController
+{
+    public function actionAdd()
+    {
+        $id = Yii::$app->request->get('id');
+        $product = Product::findOne($id);
+        if (empty($product)) return false;
+        $session = Yii::$app->session;
+        $session->open();
+        $cart = new Cart();
+        $cart->addToCart($product);
+        $this->layout = false;
+        return $this->render('cart-modal', compact('session'));
+    }
+
+    public function actionClear()
+    {
+        $session = Yii::$app->session;
+        $session->open();
+        $session->remove('cart');
+        $session->remove('cart.qty');
+        $session->remove('cart.sum');
+        $this->layout = false;
+        return $this->render('cart-modal', compact('session'));
+    }
+}
