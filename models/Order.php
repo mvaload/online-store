1873b8b243899e866e9930d17fb1749811ed4a5f
mvaload <mvaload@gmail.com>
2020-05-11 12:31:32 +0300

    added models order

---
+++ models/Order.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+use \yii\db\ActiveRecord;
+use yii\behaviors\TimestampBehavior;
+use yii\db\Expression;
+
+/**
+ * This is the model class for table "order".
+ *
+ * @property int $id
+ * @property string|null $created_at
+ * @property string|null $updated_at
+ * @property int|null $qty
+ * @property float|null $sum
+ * @property string|null $status
+ * @property string|null $name
+ * @property string|null $email
+ * @property string|null $phone
+ * @property string|null $address
+ */
+class Order extends ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'order';
+    }
+
+    public function behaviors()
+    {
+        return [
+            [
+                'class' => TimestampBehavior::className(),
+                'attributes' => [
+                    ActiveRecord::EVENT_BEFORE_INSERT => ['created_at', 'updated_at'],
+                    ActiveRecord::EVENT_BEFORE_UPDATE => ['updated_at'],
+                ],
+                // если вместо метки времени UNIX используется datetime:
+                 'value' => new Expression('NOW()'),
+            ],
+        ];
+    }
+
+    public function getOrderItems()
+    {
+        return $this->hasMany(OrderItems::className(), ['order_id' => 'id']);
+    }
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['name', 'email', 'phone', 'address'], 'required'],
+            [['created_at', 'updated_at'], 'safe'],
+            [['qty'], 'integer'],
+            [['sum'], 'number'],
+            [['status'], 'boolean'],
+            [['name', 'email', 'phone', 'address'], 'string', 'max' => 255],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'name' => 'Имя',
+            'email' => 'E-mail',
+            'phone' => 'Телефон',
+            'address' => 'Адрес',
+        ];
+    }
+
+    public function saveOrderItems($items, $order_id)
+    {
+        foreach ($items as $id => $item) {
+            $order_items = new OrderItems();
+            $order_items->order_id = $order_id;
+            $order_items->product_id = $id;
+            $order_items->name = $item['name'];
+            $order_items->price = $item['price'];
+            $order_items->qty_item = $item['qty'];
+            $order_items->sum_item = $item['qty'] * $item['price'];
+            $order_items->save();
+        }
+    }
+}
---
+++ models/OrderItems.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+use \yii\db\ActiveRecord;
+
+/**
+ * This is the model class for table "order_items".
+ *
+ * @property int $id
+ * @property int|null $order_id
+ * @property int|null $product_id
+ * @property string|null $name
+ * @property float|null $price
+ * @property int|null $qty_item
+ * @property int|null $sum_item
+ */
+class OrderItems extends ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'order_items';
+    }
+    public function getOrder()
+    {
+        return $this->hasOne(Order::className(), ['id' => 'order_id']);
+    }
+
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['order_id', 'product_id', 'name', 'price', 'qty_item', 'sum_item'], 'required'],
+            [['order_id', 'product_id', 'qty_item', 'sum_item'], 'integer'],
+            [['price'], 'number'],
+            [['name'], 'string', 'max' => 255],
+        ];
+    }
+
+}
